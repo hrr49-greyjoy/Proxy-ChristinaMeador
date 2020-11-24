c611e5e11a13290bd9ed97d31e35138cce04b2fe
Christina Meador <chrissymeador@gmail.com>
2020-11-24 07:20:45 -0600

    proxy template rendering

--- server/config/services.js
+++ server/config/services.js
@@ -42,23 +42,23 @@
 
 module.exports = {
   service1: {
-    api: 'FIX_ME',
+    api: '/reviews',
     url: 'http://localhost:3001',
-    bundle: 'app.js',
+    bundle: '/dist/main.js',
   },
   service2: {
-    api: 'FIX_ME',
-    url: 'http://localhost:3002',
-    bundle: 'app.js',
+    api: '/campsites/*',
+    url: 'http://localhost:3005',
+    bundle: 'bundle.js',
   },
   service3: {
-    api: 'FIX_ME',
-    url: 'http://localhost:3003',
-    bundle: 'app.js',
+    api: '/images',
+    url: 'http://localhost:8080',
+    bundle: 'bundle.js',
   },
   service4: {
-    api: 'FIX_ME',
+    api: '/camps/',
     url: 'http://localhost:3004',
-    bundle: 'app.js',
+    bundle: 'bundle.js',
   },
 };
